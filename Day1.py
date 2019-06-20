0f63bde82ce31b925bd521f994efeff1dba9a170
Suresh Reddy <49857619+1547015@users.noreply.github.com>
2019-06-20 16:42:48 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,17 @@
+
+ # Sample program created for printing the statements
+
+a = 'boyina'
+b = 'engineer'
+print("welcome to python")
+print("hello")
+print("good morning")
+# print(a+b)
+c = 'program'
+f = 10
+d = (a +' '+ b +' '+ c) * f
+print(d)
+print("My name is:"+a)
+print("My phone is:"+b)
+
+
